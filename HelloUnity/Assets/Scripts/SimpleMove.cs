5d419bc3662677c02e4ff36c66311c9d90505918
haley.phillips <haleyphillips928@gmail.com>
2019-09-23 20:54:08 -0500

    begin simple move script

---
+++ HelloUnity/Assets/Scripts/SimpleMove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleMove : MonoBehaviour
+{
+    public Vector3 direction = new Vector3(1, 1, 0); //need to do inline initialization in unity
+    public float speed = 10;
+    protected Vector3 moveTranslation;
+
+    void Awake()
+    {
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //time corrected movement
+        this.moveTranslation = new Vector3(this.direction.x, this.direction.y) * Time.deltaTime * this.speed;
+
+        //move
+        this.transform.position += new Vector3(moveTranslation.x, moveTranslation.y);
+    }
+}
