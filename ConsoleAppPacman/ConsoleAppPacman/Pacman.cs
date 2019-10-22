6ed3741eb216a32ebde60eb06b95ffe0166f0d80
haley.phillips <haleyphillips928@gmail.com>
2019-10-21 19:22:07 -0500

    added console pacman

---
+++ ConsoleAppPacman/ConsoleAppPacman/Pacman.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleAppPacman
+{
+    public enum PacmanState { Spawning, Still, Chomping, SuperPacMan }
+
+    class Pacman
+    {
+        protected PacmanState _state; //private instance data member for this pacman
+
+        public PacmanState State
+        {
+            get { return _state; }
+            set {
+                if(_state != value)
+                {
+                    this.Log(string.Format("{0} was {1} now {2}", this.ToString(),value,value));
+
+                    _state = value;
+                }
+      
+            }
+        }
+
+        public Pacman()
+        {
+            this._state = PacmanState.Spawning;
+        }
+
+        public virtual void Log (string s)
+        {
+            Console.WriteLine(s);
+        }
+
+    }
+}
