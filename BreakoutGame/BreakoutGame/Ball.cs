10b4618a112abaed1011c8b91a1babb219543abb
Haley Phillips <haleyphillips928@gmail.com>
2019-10-28 19:04:54 -0500

    init

---
+++ BreakoutGame/BreakoutGame/Ball.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BreakoutGame
+{
+    class Ball : DrawableSprite
+    {
+        public Ball(Game1 game): base(game)
+        {
+
+        }
+
+#if DEBUG
+
+#endif
+    }
+
+    protected void LoadContent()
+    {
+        
+    }
+}
